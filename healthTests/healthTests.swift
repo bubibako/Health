562c73d571a0973d2d78a43f02710c09f7c5dcf9
Arthur <artrampnau@mail.ru>
2025-01-25 18:02:35 +0300

    Initial Commit

---
+++ healthTests/healthTests.swift
@@ -0,0 +1,17 @@
+//
+//  healthTests.swift
+//  healthTests
+//
+//  Created by Arthur Trampnau on 25/01/25.
+//
+
+import Testing
+@testable import health
+
+struct healthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
